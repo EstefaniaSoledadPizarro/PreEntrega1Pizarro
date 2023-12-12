1ebf0d8b6fb3fa0ca36bb417200f0e03b5f5e3d5
Soledad Pizarro <soledad.pizarro21@gmail.com>
2023-12-12 00:36:46 -0300

    componentes

--- PreEntrega1PizarroEstefania/src/App.jsx
+++ PreEntrega1PizarroEstefania/src/App.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-
-
 import ItemListContainer from "./Components/ItemListContainer/ItemListContainer";
 import './App.css';
-import NavBar from "./Components/NavBar/NavBar";
+import NavBar from "./components/NavBar/NavBar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Error from './Components/Error'
 
-function App () {
+
+function App() {
   return (
     <div className="App">
-<NavBar/>
-<ItemListContainer greeting= 'Bienvenidos a Tienda Mate'/>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<ItemListContainer />} />
+          <Route path='/category/:id' element={<ItemListContainer />} />
+          <Route path='/item/:id' element={<ItemDetailContainer />} />
+          <Route path='*' element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
